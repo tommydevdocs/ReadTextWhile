9b9369f2454c33fbf77fa0e5977335f852e61c60
tommydevdocs <tommydevdocs@gmail.com>
2015-06-17 17:50:59 -0500

    Initial Commit

---
+++ ReadTextFileWhile/Module1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Dim myReader As StreamReader = New StreamReader("Values.txt")
+
+        Dim line As String = ""
+
+        While Not IsNothing(line)
+
+            line = myReader.ReadLine()
+            If Not IsNothing(line) Then
+                Console.WriteLine(line)
+            End If
+
+        End While
+
+        myReader.Close()
+        Console.ReadLine()
+    End Sub
+
+End Module
